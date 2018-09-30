94dac559beebdd1025d805c26f11ebf1f6632471
Dana Palmer <roiai4@hotmail.com>
2018-09-30 23:49:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,7 @@ puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
-    end
+end
 
 def input_to_index(user_input)
   user_input.to_i - 1
