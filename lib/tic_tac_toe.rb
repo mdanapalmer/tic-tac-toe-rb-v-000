3db47e57316ce32ec8c857b5e9ae6d6e87084deb
Dana Palmer <roiai4@hotmail.com>
2018-09-30 23:47:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,9 +26,7 @@ def move(board, index, player)
 end
 
 def position_taken?(board, index)
-   if board[index] == "" || board[index] == " " || board[index] == nil
-    false
-   else board[index] =="X" || board[index] =="O"
+  board[index] =="X" || board[index] =="O"
 end
 
 def valid_move?(board, index)
